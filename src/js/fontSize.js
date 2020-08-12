3a9ce1c030c264ac322affe22c6527aad8bf2cc0
HDD0207 <1285279121@qq.com>
2020-08-12 15:43:03 +0800

    fontSize change

---
+++ src/js/fontSize.js
@@ -0,0 +1,4 @@
+window.onresize = function () {
+    document.querySelector("html").style.fontSize = window.innerWidth / 375 * 16 + "px";
+}
+console.log(document.querySelector("html").style.fontSize);

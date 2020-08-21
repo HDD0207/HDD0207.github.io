a4b54252d6149e3d22a26287dc5cf5f8ecbbe8d0
HDD0207 <1285279121@qq.com>
2020-08-21 17:09:33 +0800

    修改了样式

--- src/js/fontSize.js
+++ src/js/fontSize.js
@@ -1,4 +1,4 @@
-window.onresize = function () {
-    document.querySelector("html").style.fontSize = window.innerWidth / 375 * 16 + "px";
-}
-console.log(document.querySelector("html").style.fontSize);
+// window.onresize = function () {
+//     document.querySelector("html").style.fontSize = window.innerWidth / 375 * 16 + "px";
+// }
+// console.log(document.querySelector("html").style.fontSize);

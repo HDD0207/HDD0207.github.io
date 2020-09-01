a9bafc4c0146efd896900580b9e54f2a0de31a89
HDD0207 <1285279121@qq.com>
2020-09-01 19:14:31 +0800

    新增了css3项目

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,28 @@
 $(() => {
-    let height = $("main").outerHeight(true);
     let index = 0;
-    $(".goTop").hide(500);
-    $("main").scroll(function (e) {
-        let scrollTop = $("main").scrollTop();
+    let goTop = $(".goTop")
+    goTop.hide(500);
+    $(document).scroll(function () {
+        let scrollTop = $(document).scrollTop();
         if (scrollTop > 200) {
-            $(".goTop").show(500);
-        } else {
-            $(".goTop").hide(500);
+            goTop.show(500);
+        } else{
+            goTop.hide(500);
         }
     })
-    $(".goTop").click(function () {
+    goTop.click(function () {
         index = 0;
-        $("main").animate({ scrollTop: 0 }, 500);
+        $("html,body").animate({ scrollTop: 0 }, 500);
     })
-    function init() {
-        let width = $(".v-title-item").width();
-        let deg = Math.PI / 180;
-        // console.log(deg * 30);
-        let top = Math.sin(deg * 45);
-        // console.log(top)
-        let left = Math.cos(deg * 45)
-
-        for (let i = 0; i < $(".v-title-item li").length; i++) {
-            $(".v-title-item li").eq(i).css({ transform: `translate(${-left * i}rem, ${top * i}rem)` }) 
+    let bool = true;
+    $(".list-nav a").click(function () {
+        let scrollTop = $($(this).attr("href")).offset().top ;
+        if (bool) {
+            bool = false;
+            $("html,body").animate({ scrollTop }, 500, function () {
+                bool = true;
+            })
         }
-    }
-    init();
-    // $(".list-item").click(function () {
-    //     if (index == $(this).attr("data-index")) return;
-    //     index = $(this).attr("data-index");
-    //     let scrollHeight = index * (height + 10);
-    //     if ($("main").scrollTop() == scrollHeight) return;
-    //     $("main").animate({ scrollTop: scrollHeight }, 500)
-    // })
+    })
 
 })

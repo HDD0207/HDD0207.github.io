c72426bdecb128ad0b1c6f9c26f16846f9629080
HDD0207 <1285279121@qq.com>
2020-09-02 20:32:06 +0800

    压缩了图片

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,16 @@
 $(() => {
-    // $(window).on("load",function(){
-    //     $(".v-mask").fadeOut(500)
-    //     $("html,body").css({overflowY:"auto"})
-    // })
-    // window.onload = new function(){
-    //     $(".v-mask").fadeOut(500)
-    //     $("html,body").css({overflowY:"auto"})
-    // }
     if (window.addEventListener) {
         window.addEventListener("load", function () {
-            console.log(1);
             $(".v-mask").fadeOut(500);
             $("html,body").css({ overflowY: "auto" });
         }, false);
     } else if (window.attachEvent) {
         window.attachEvent("onload", function () {
-            console.log(2);
             $(".v-mask").fadeOut(500);
             $("html,body").css({ overflowY: "auto" });
         });
     } else if (window.onload) {
         window.onload = function () {
-            console.log(3);
             $(".v-mask").fadeOut(500);
             $("html,body").css({ overflowY: "auto" });
         };

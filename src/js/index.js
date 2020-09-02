2c52315a1918f7bf9a077eb66bd8fa0132b4c97b
HDD0207 <1285279121@qq.com>
2020-09-02 19:33:10 +0800

    test

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,31 @@
 $(() => {
-    $(window).on("load",function(){
+    // $(window).on("load",function(){
+    //     $(".v-mask").fadeOut(500)
+    //     $("html,body").css({overflowY:"auto"})
+    // })
+    window.onload = new function(){
         $(".v-mask").fadeOut(500)
         $("html,body").css({overflowY:"auto"})
-    })
+    }
+    // if (window.addEventListener) {
+    //     window.addEventListener("load", function () {
+    //         console.log(1);
+    //         $(".v-mask").fadeOut(500);
+    //         $("html,body").css({ overflowY: "auto" });
+    //     }, false);
+    // } else if (window.attachEvent) {
+    //     window.attachEvent("onload", function () {
+    //         console.log(2);
+    //         $(".v-mask").fadeOut(500);
+    //         $("html,body").css({ overflowY: "auto" });
+    //     });
+    // } else if (window.onload) {
+    //     window.onload = function () {
+    //         console.log(3);
+    //         $(".v-mask").fadeOut(500);
+    //         $("html,body").css({ overflowY: "auto" });
+    //     };
+    // }
     let index = 0;
     let goTop = $(".goTop")
     goTop.hide(500);
@@ -10,7 +33,7 @@ $(() => {
         let scrollTop = $(document).scrollTop();
         if (scrollTop > 200) {
             goTop.show(500);
-        } else{
+        } else {
             goTop.hide(500);
         }
     })
@@ -20,7 +43,7 @@ $(() => {
     })
     let bool = true;
     $(".list-nav a").click(function () {
-        let scrollTop = $($(this).attr("href")).offset().top ;
+        let scrollTop = $($(this).attr("href")).offset().top;
         if (bool) {
             bool = false;
             $("html,body").animate({ scrollTop }, 500, function () {
